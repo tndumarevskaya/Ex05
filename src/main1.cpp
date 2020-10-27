98fa9c1cafde8992ef46b3abf2ebc37ca8e7cde5
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-10-27 17:38:27 +0300

    first

---
+++ src/main1.cpp
@@ -0,0 +1,10 @@
+#include "task1.h"
+
+int main()
+{
+    cout << less(1, 2) << endl;
+    cout << less(1.2, 3.2) << endl;
+    cout << less('a', 'b') << endl;
+    cout << less('abba', 'baab');
+    return 0;
+}
