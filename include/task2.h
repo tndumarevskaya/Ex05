7011ba0bdac668791fdaed6bd4c5909677b93abc
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-10-27 17:29:28 +0300

    fix

--- include/task2.h
+++ include/task2.h
@@ -1,15 +1,14 @@
-﻿#ifndef TASK2_H
+#ifndef TASK2_H
 #define TASK2_H
 
 #include <iostream>
 using namespace std;
 
 template<typename T>
-T averageArr(T* arr, int size)
+T averageArr(T* arr, const int size)
 {
-	int sum = 0;
-	for (unsigned int i = 0; i < size; i++)
-		sum += arr[i];
-	return sum / size;
+	for (int i = 1; i < size; i++)
+		arr[0] += arr[i];
+	return arr[0] / size;
 }
 #endif
