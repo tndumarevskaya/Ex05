84bd1c9af6be367009c23a44ef2472d45d70c314
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-10-27 18:52:31 +0300

    fix

--- src/main1.cpp
+++ src/main1.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
-    cout << less(1, 2) << endl;
-    cout << less(1.2, 3.2) << endl;
-    cout << less('a', 'b') << endl;
-    cout << less('abba', 'baab');
+    int a = 2, b = 3;
+    double c = 2.7, d = 1.8;
+    const char str[] = "abba";
+    const char str2[] = "baab";
+    char x = 'a';
+    char y = 'b';
+    cout << less(a, b) << endl << less(c, d) << endl << less(str, str2) << endl << less(x, y);
     return 0;
 }
