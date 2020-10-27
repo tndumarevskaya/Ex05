424a6f84029265f60d94373a08637b46e70719d9
tndumarevskaya <63363543+tndumarevskaya@users.noreply.github.com>
2020-10-27 18:56:06 +0300

    fix

--- src/main2.cpp
+++ src/main2.cpp
@@ -4,7 +4,7 @@ int main()
 {
 	const int size = 6;
 	int arr[size]{ 4, 3, 2, 2, 2, -1 };
-	float arr2[size]{ 4.0, 3.3, 2.1, 2.4, 2, -1.6 };
+	double arr2[size]{ 4.0, 3.3, 2.1, 2.4, 2, -1.6 };
 	cout << averageArr(arr, size) << endl;
 	cout << averageArr(arr2, size);
 	return 0;
